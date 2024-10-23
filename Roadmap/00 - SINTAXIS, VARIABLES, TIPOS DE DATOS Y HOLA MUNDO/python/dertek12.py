f82a29fa9316eeba6a1c27ec6ebb5c42130c6086
dertek12 <dertek.giova@gmail.com>
2024-10-23 13:22:14 -0600

    Create dertek12.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dertek12.py
@@ -0,0 +1,18 @@
+# http://python.org
+
+# comentario en una linea 
+"""
+Esto
+tambien es un
+comentario en varias lineas
+""" 
+
+'''
+esto tambien mijo
+es varias lineas
+'''
+my_variable = "mi variable" 
+my_variable = "este es otro nuevo valor de mi variable" 
+
+MY_CONSTANT = "Mi constante" # por convencion 
+MY_CONSTANT = "ASASS22222222" 
